11103718dee4b3e4a71cfd015f029bfbc9f59798
Iam lion <Iamlion15@gmail.com>
2023-10-04 03:21:45 -0700

    added app loading

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,28 @@ import Head from "next/head"
 import Link from "next/link"
 import Router from "next/router"
 import nProgress from "nprogress"
+
+Router.onRouteChangeStart = url => nProgress.start()
+Router.onRouteChangeComplete = url => nProgress.done()
+Router.onRouteChangeError = url => nProgress.done()
 const Layout = (props) => {
     const head = () => (
-        <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-            crossorigin="anonymous" />
+        <>
+            <link
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                crossorigin="anonymous" />
+            <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+                integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+                crossorigin="anonymous" y
+                referrerpolicy="no-referrer"
+            />
+        </>
     )
+
     const nav = () => (
         <ul className="nav nav-tabs bg-warning">
             <li className="nav-item">
